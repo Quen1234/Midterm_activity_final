e4a43bebf46bab88534cf543d229f81f7bf5f736
jayca <hermosurajayca420@gmail.com>
2026-04-16 14:41:37 +0800

    fixed listahan

--- app/Models/ListahanModel.php
+++ app/Models/ListahanModel.php
@@ -9,6 +9,6 @@ class ListahanModel extends Model
     protected $table = 'listahan';
     protected $primaryKey = 'id';
     protected $allowedFields = ['customer_name', 'items', 'amount', 'status'];
-    protected $useTimestamps = true;
+    protected $useTimestamps = false;
     protected $createdField  = 'created_at';
 }
